f4fc678991e4582e4ef56d6ec90485bbc293e3ed
Martin M Reed <martin.reed@hallowtech.com>
2020-09-02 16:28:59 -0300

    Adding Github Pages.

---
+++ precache-manifest.afa88b4c094c0f7a3ea520db86fa0ea0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e644aca86911980885eb248b2e486b",
+    "url": "/aws-iot-baseline/index.html"
+  },
+  {
+    "revision": "002352a6d49407d19a62",
+    "url": "/aws-iot-baseline/static/css/main.9207781b.chunk.css"
+  },
+  {
+    "revision": "093dc5312ce9524441bb",
+    "url": "/aws-iot-baseline/static/js/2.88ecbacb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/aws-iot-baseline/static/js/2.88ecbacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002352a6d49407d19a62",
+    "url": "/aws-iot-baseline/static/js/main.5a062b3d.chunk.js"
+  },
+  {
+    "revision": "ec7f45df7055f7de2a22",
+    "url": "/aws-iot-baseline/static/js/runtime-main.a97fc3d1.js"
+  }
+]);
